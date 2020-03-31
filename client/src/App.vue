55a56f9cd79ea6f19d3bc772aa25122db4b7c9d9
Yves Boillat <yboillat@hsr.ch>
2020-03-31 23:18:10 +0200

    add app structure

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="page-container">
+      <md-app md-waterfall md-mode="fixed">
+        <md-app-toolbar class="md-primary" >
+          <span class="md-title">Coastline Demo</span>
+        </md-app-toolbar>
+        <md-app-content>
+          <router-view/>
+        </md-app-content>
+      </md-app>
     </div>
-    <router-view/>
+    <md-bottom-bar md-sync-route>
+      <md-bottom-bar-item to="/" exact md-label="Home" md-icon="home"></md-bottom-bar-item>
+      <md-bottom-bar-item to="/about" md-label="People" md-icon="people"></md-bottom-bar-item>
+    </md-bottom-bar>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  #app {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
-}
+
+  .md-app {
+    border: 1px solid rgba(#000, .12);
+    height: calc(100vh - 56px);
+  }
+
+  .md-bottom-bar {
+    position: fixed;
+    bottom: 0;
+  }
 </style>
+
+<script>
+export default {
+  name: 'AppBody',
+};
+</script>

ecfa956ccaa72cf77d5c3931b696a9f91972650c
Yves Boillat <yboillat@hsr.ch>
2020-04-01 00:26:00 +0200

    add example user list from mock api

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,59 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div>
+      <md-button v-on:click="loadList" class="md-raised md-primary">Load list</md-button>
+    </div>
+    <md-list class="md-triple-line">
+      <div v-for="item in testList" :key="item.firstName">
+        <md-list-item >
+          <md-avatar class="md-avatar-icon md-primary">{{ item.firstName.toString().slice(0,1) + item.lastName.toString().slice(0,1) }}</md-avatar>
+
+          <div class="md-list-item-text">
+            <span>{{ item.firstName + ' ' + item.lastName }}</span>
+          </div>
+        </md-list-item>
+        <md-divider class="md-inset"></md-divider>
+      </div>
+    </md-list>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'TripleLine',
+  data() {
+    return {
+      testList: [],
+    };
+  },
+  created() {
+    // fetch the data when the view is created and the data is
+    // already being observed
+    this.loadList();
+  },
+  watch: {
+    // call again the method if the route changes
+    $route: 'loadList',
+  },
+  methods: {
+    loadList() {
+      // TODO: use API URL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      fetch('https://yoloo.free.beeceptor.com', {
+        method: 'GET',
+      })
+        .then((response) => response.json())
+        .then((result) => { this.testList = result; });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .md-list {
+    width: 100%;
+    max-width: 100%;
+    display: inline-block;
+    vertical-align: top;
+    border: 1px solid rgba(#000, .12);
+  }
+</style>

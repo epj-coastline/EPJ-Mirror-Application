b30755291e09f951ab3426317a7f46b178b59406
Fabian Germann <fabian.germann@hsr.ch>
2020-04-13 11:53:26 +0200

    change comment in Configuration.ts

--- client/src/Configuration.ts
+++ client/src/Configuration.ts
@@ -1,14 +1,7 @@
 /*
-NEW ENVIRONMENT VARIABLES NEED ADJUSTMENTS IN entrypoint.sh AS WELL!
-
-The local variables are defined in the .env files and can be
+New environment variables for production need to be set in entrypoint.sh.
+Variables during development are defined in the .env files and can be
 overwritten by creating a .env.local file which is ignored by git.
-
-Note on terrible code below:
-We need to check process.env.NODE_ENV our self because webpack does some magic and includes env
-and replaces the values we don't want in production. Also this form is the only way that we
-don't include local code in production.
-
  */
 
 export default class Configuration {

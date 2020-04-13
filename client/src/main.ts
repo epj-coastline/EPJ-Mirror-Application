278b559650671206652ded0ce718bf3f38ed8a9a
Fabian Germann <fabian.germann@hsr.ch>
2020-04-13 11:48:01 +0200

    removed dotenv as it's already included in vue

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,12 +2,9 @@ import Vue from 'vue';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
-import dotenv from 'dotenv';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
-
-dotenv.config();
 
 Vue.config.productionTip = false;
 

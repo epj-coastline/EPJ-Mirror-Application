4cdf88df823064c635de77506cfe348e50b476e7
luthda <dluthige@hsr.ch>
2020-04-29 12:04:03 +0200

    add module entity and dependencies

---
+++ server/CoastlineServer.DAL/Entities/Confirmation.cs
@@ -0,0 +1,11 @@
+﻿namespace CoastlineServer.DAL.Entities
+{
+    public class Confirmation
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int StrengthId { get; set; }
+        public Strength Strength { get; set; }
+    }
+}

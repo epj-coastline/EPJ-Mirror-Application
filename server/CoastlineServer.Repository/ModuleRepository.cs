235e4583597e63ae506f23458e79057b6ed9b5b8
luthda <dluthige@hsr.ch>
2020-05-18 14:06:19 +0200

    remove usings

--- server/CoastlineServer.Repository/ModuleRepository.cs
+++ server/CoastlineServer.Repository/ModuleRepository.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using CoastlineServer.DAL.Context;
 using CoastlineServer.DAL.Entities;
